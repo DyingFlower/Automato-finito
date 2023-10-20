f175362afb8922ea79240cebe3ee11cc6c37b0a4
DyingFlower <112014310+DyingFlower@users.noreply.github.com>
2023-10-20 11:52:37 -0300

    Add files via upload

---
+++ paradigmas.c
@@ -0,0 +1,30 @@
+ // Trabalho paradigmas
+ 
+ #include<stdio.h>
+ #include<ctype.h>
+ //
+ char lex;
+ char lexlen(100);
+ 
+ // tokens
+ /* ver arquivos em C*/
+ /* mais tarde envio um mais pensado, sem noção de como fazer agora*/
+ #define And 10
+ #define Or  11
+ #define Not  12
+ #define True  13
+ #define False  14
+ #define Ident  15
+ #define Com  16
+ #define equa  17
+ 
+ //funções
+ char getchar();
+ char addchar();
+ int lookup();
+ 
+ int main(void){
+ 	
+ }
+ 
+ 
